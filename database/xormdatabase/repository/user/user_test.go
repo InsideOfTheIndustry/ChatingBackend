b9779a7bcbf0102441d04e33f7c272a6707e5914
zhujunjie <zhujunjie@egova.com.cn>
2021-06-29 21:37:38 +0800

    update: friend select

--- database/xormdatabase/repository/user/user_test.go
+++ database/xormdatabase/repository/user/user_test.go
@@ -11,33 +11,30 @@ package user
 
 import (
 	"chatting/database/xormdatabase"
-	"chatting/domain/user/entity"
 	"chatting/infrastructure/configServer"
 	"chatting/infrastructure/logServer"
+	"fmt"
 	"testing"
 )
 
 func TestCreate(t *testing.T) {
 	// 读取配置文件
-	configServer.InitConfig("../../../../config/config.json")
+	// configServer.InitConfig("../../../../config/config.json")
 	// 测试数据库
-	var userdao = UserRepository{}
+	configServer.InitConfig("../../../../config/config.yaml")
+	logServer.SetFileLevel("info") // 设置日志等级)
 	xormdatabase.InitXormEngine()
-	userdao.XormEngine = xormdatabase.DBEngine
-	var userinfo = &entity.UserInfo{
-		Avatar:      "http://image13.m1905.cn/mdb/uploadfile/2018/1121/thumb_1_300_410_20181121095100733988.jpg",
-		UserAccount: 100009,
-	}
-	// account, _ := userdao.Create(&userinfo)
+	// t, _ := userdao.Create(&userinfo)
 	// users, err := userdao.Query(12313131)
 	// ifre, err := userdao.QueryEmailIfAlreadyUse("12138")
-	friends, err := userdao.QueryFriends(10009)
-	err = userdao.UpdateAvatar(userinfo)
+
 	// userdao.Update(&userinfo)
 
 	//logServer.Info("用户账号为:%v", account)
 	//logServer.Info("用户是否存在:%v,%v", *users, err)
 	// logServer.Info("邮箱是否已被注册:%v", ifre)
-	logServer.Info("错误信息:%v", err)
-	logServer.Info("好友信息:%v", friends)
+	ur := UserRepository{xormdatabase.DBEngine}
+	h, _ := ur.QueryFriends(100009)
+	fmt.Printf("内容为:%v", h)
+
 }
